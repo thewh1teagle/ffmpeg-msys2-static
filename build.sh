f8eb1f1eb1d2b28be65a41b5ad54b9a4e9b68edd
thewh1teagle <61390950+thewh1teagle@users.noreply.github.com>
2024-01-31 22:20:02 +0200

    update

--- build.sh
+++ build.sh
@@ -2,9 +2,12 @@
 # Setup av2 ffmpeg
 FFMPEG_NAME=ffmpeg-6.0-windows-desktop-clang-default
 FFMPEG_URL="https://master.dl.sourceforge.net/project/avbuild/windows-desktop/$FFMPEG_NAME.tar.xz?viasf=1"
-wget -nc --show-progress $FFMPEG_URL -O $FFMPEG_NAME.tar.xz
-tar xf $FFMPEG_NAME.tar.xz
-rm -f $FFMPEG_NAME.tar.xz
+if [ ! -d $FFMPEG_NAME ]; then
+    wget -nc --show-progress $FFMPEG_URL -O $FFMPEG_NAME.tar.xz
+    tar xf $FFMPEG_NAME.tar.xz
+    rm -f $FFMPEG_NAME.tar.xz
+fi
+
 
 # Build
 export FFMPEG_DIR=$FFMPEG_NAME
